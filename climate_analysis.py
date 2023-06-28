6484c28da587c708c63299c2c6efe5b641f2f9ea
Meg Perry-Duxbury <29304315+megvdeelen@users.noreply.github.com>
2023-06-28 11:32:42 +0100

    #2 - add missing module doctring

--- climate_analysis.py
+++ climate_analysis.py
@@ -1,10 +1,12 @@
+"""Performs conversion between different temperature scales."""
+
 SHIFT = 3
 COMMENT = '#'
 climate_data = open('data/sc_climate_data_10.csv', 'r')
 
 
 def FahrToCelsius(fahr):
-    """COnverts fahrenehit to celsius
+    """COnverts fahrenhit to celsius
 
     Args:
         fahr (float): temperature in fahrenheit
